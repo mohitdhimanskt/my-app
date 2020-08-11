e36411677265b5384a5a50ef08ead097d1f99060
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-08-11 11:21:00 +0530

    Add to file

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <v-app-bar app color="white">
+      <div class="d-flex align-center">
+        <v-img
+          alt="Far Out Himalayas Logo"
+          class="shrink mr-2"
+          contain
+          src="https://farout.herokuapp.com/static/FOH.PNG"
+          transition="scale-transition"
+          width="40"
+        />
+        <v-app-title class="headline text-uppercase">
+          <span class="font-weight-light">Far Out Himalayas</span>
+        </v-app-title>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn>
+        <span class="mr-2">ABOUT US</span>
+      </v-btn>
+      <v-btn>
+        <span class="mr-2">Login</span>
+      </v-btn>
+      <v-btn>
+        <span class="mr-2">SIGN UP</span>
+      </v-btn>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  data: function () {
+    return {
+      name: "Mohit",
+    };
+  },
+};
+</script>
+
+
+
+
